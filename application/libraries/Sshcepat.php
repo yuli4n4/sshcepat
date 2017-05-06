921cc785e8a9210207743d1480b1098c19d854c3
AdipatiArya <jawircodes@gmail.com>
2017-05-06 12:16:14 -0800

    Add library sshcepat

--- application/libraries/Sshcepat.php
+++ application/libraries/Sshcepat.php
@@ -1,32 +1,38 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . APPPATH . '/third_party/phpseclib');
-include(APPPATH . '/third_party/phpseclib/Net/SSH2.php');
 class Sshcepat {
 	private $ssh, $hostName, $rootPass, $result;
-	public function result() {
+	public function __construct()
+	{
+		set_include_path(get_include_path() . PATH_SEPARATOR . APPPATH . '/third_party/phpseclib');
+		include(APPPATH . '/third_party/phpseclib/Net/SSH2.php');
+	}
+	public function result()
+	{
 		return $this -> result;
 	}
-	public function set_hostname($host, $root) {
+	public function set_hostname($host, $root)
+	{
 		$this -> ssh = new Net_SSH2($host);
-                if (!$this -> ssh-> login('root', $root))
+		if (!$this -> ssh-> login('root', $root))
 		{
 			exit (json_encode(
 				array('status' => 'Failed', 'result' => 'Kesalahan root password')
 			));
-                }
+		}
 		$this->hostName = $host; $this->rootPass=$root;
 	}
-	public function set_user($user, $pass, $limit) {
+	public function set_user($user, $pass, $limit)
+	{
 		$ssh  =  $this -> ssh;
 		$host = $this -> hostName;
 		$root = $this -> rootPass;
 
-		if (empty($host) && empty ($root)) {
+		if (empty($host) && empty ($root))
+		{
 			exit (json_encode(
                                 array('status' => 'Failed', 'result' => 'Hostname not intiated')
                         ));
 		}
-		//echo $host; echo $root;
 		$ssh->exec("useradd -e \"$limit days\" -s /bin/false -M $user ");
 		$ssh->enablePTY();
 		$ssh->exec("passwd $user");
