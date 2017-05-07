da0ea0282c35df1d13b8aa4bbde33a9260a8af24
AdipatiArya <jawircodes@gmail.com>
2017-05-07 10:34:42 -0800

    add table

--- database.sql
+++ database.sql
@@ -94,6 +94,32 @@ LOCK TABLES `server` WRITE;
 INSERT INTO `server` VALUES (1,'128.99.117.44','kampret123',5,'S_DO-01','Singapore','22','443','Asia'),(2,'128.99.117.45','pNLKcDG',10,'S_DO-02','Indonesia','22','443','Asia'),(3,'128.99.117.47','okwelkmN',10,'S_DO-03','Japan','22','443','Asia'),(4,'128.99.117.46','okwelkmnK',10,'S_DO-04','Japan','22','443','Asia'),(6,'api.sshcepat.com','kampret123',7,'SGDO-04','Singapore','22','443','Asia'),(7,'122.33.44.454','Klas',34,'SGDO-04','Singapore','22','443','Asia'),(8,'124.33.44.454','Klas',34,'SGDO-04','Singapore','22','443','Asia'),(9,'142.33.44.454','Klas',34,'SGDO-04','Singapore','22','443','Asia'),(10,'122.33.444.454','Klas',34,'SGDO-54','Singapore','22','443','Asia'),(11,'122.33.44.154','Klas',34,'SGDO-04','Singapore','22','443','Asia'),(12,'122.33.464.454','Klas',34,'SGDO-04','Singapore','22','443','Asia'),(13,'122.33.445.454','Klas',34,'SGDO-04','Singapore','22','443','Asia');
 /*!40000 ALTER TABLE `server` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `website`
+--
+
+DROP TABLE IF EXISTS `website`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `website` (
+  `brand` varchar(20) NOT NULL DEFAULT 'YOURWEB',
+  `author` varchar(20) NOT NULL,
+  `title` text,
+  `description` text NOT NULL,
+  PRIMARY KEY (`brand`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `website`
+--
+
+LOCK TABLES `website` WRITE;
+/*!40000 ALTER TABLE `website` DISABLE KEYS */;
+INSERT INTO `website` VALUES ('YOURWEB','SSHcepat.com','Free SSH Account','Fast Premium SSH Account Server Singapore, US, Japan, Netherlands, France, Indonesia, Vietnam, Germany,  Russia, Canada etc with Unlimited Data and High Speed Connection');
+/*!40000 ALTER TABLE `website` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -104,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-07  7:15:05
+-- Dump completed on 2017-05-07 10:34:15
