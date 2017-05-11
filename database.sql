8747145541ccd3f20c3abdde920cc913b211014f
AdipatiArya <jawircodes@gmail.com>
2017-05-11 04:22:27 -0800

    modifed

--- database.sql
+++ database.sql
@@ -81,7 +81,6 @@ CREATE TABLE `server` (
   `Location` varchar(20) DEFAULT NULL,
   `OpenSSH` varchar(20) NOT NULL DEFAULT '22',
   `Dropbear` varchar(20) NOT NULL DEFAULT '443',
-  `Cid` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`Id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -92,7 +91,7 @@ CREATE TABLE `server` (
 
 LOCK TABLES `server` WRITE;
 /*!40000 ALTER TABLE `server` DISABLE KEYS */;
-INSERT INTO `server` VALUES (1,'128.99.117.44','bangsat',5,7,'S_DO-01','Singapore','22','443','Asia'),(2,'128.99.117.45','pNLKcDG',10,7,'S_DO-02','Indonesia','22','443','Asia'),(3,'128.99.117.47','okwelkmN',10,7,'S_DO-03','Japan','22','443','Asia'),(4,'128.99.117.46','okwelkmnK',10,7,'S_DO-04','Japan','22','443','Asia'),(6,'192.168.122.22','1',7,7,'SGDO-04','Singapore','22','443','Asia'),(7,'122.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443','Asia'),(8,'124.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443','Asia'),(9,'142.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443','Asia'),(10,'122.33.444.454','Klas',34,7,'SGDO-54','Singapore','22','443','Asia'),(11,'122.33.44.154','Klas',34,7,'SGDO-04','Singapore','22','443','Asia'),(12,'122.33.464.454','Klas',34,7,'SGDO-04','Singapore','22','443','Asia'),(13,'122.33.445.454','Klas',34,7,'SGDO-04','Singapore','22','443','Asia');
+INSERT INTO `server` VALUES (1,'128.99.117.44','bangsat',5,7,'S_DO-01','Singapore','22','443'),(2,'128.99.117.45','pNLKcDG',10,7,'S_DO-02','Indonesia','22','443'),(3,'128.99.117.47','okwelkmN',10,7,'S_DO-03','Japan','22','443'),(4,'128.99.117.46','okwelkmnK',10,7,'S_DO-04','Japan','22','443'),(6,'192.168.122.22','1',7,7,'SGDO-04','Singapore','22','443'),(7,'122.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443'),(8,'124.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443'),(9,'142.33.44.454','Klas',34,7,'SGDO-04','Singapore','22','443'),(10,'122.33.444.454','Klas',34,7,'SGDO-54','Singapore','22','443'),(11,'122.33.44.154','Klas',34,7,'SGDO-04','Singapore','22','443'),(12,'122.33.464.454','Klas',34,7,'SGDO-04','Singapore','22','443'),(13,'122.33.445.454','Klas',34,7,'SGDO-04','Singapore','22','443');
 /*!40000 ALTER TABLE `server` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -157,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-11  4:16:18
+-- Dump completed on 2017-05-11  4:22:12
